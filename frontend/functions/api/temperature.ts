9bd9dc821d7baacadea258b95b2fbb80e80e33f1
CasperJ <c@sper.name>
2023-05-08 19:57:28 +0000

    get data from KV store

--- frontend/functions/api/temperature.ts
+++ frontend/functions/api/temperature.ts
@@ -1,5 +1,5 @@
 interface Env {
-	KV: string;
+	DATA: KVNamespace;
 }
 
 interface Record {
@@ -10,13 +10,9 @@ interface Record {
   
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-	//const value = await context.env.KV;
-    const readings : Record[] = [
-        { RecordedOn: new Date(2023, 4,1,12,0,0), Temperature: 10.8, Temperature2: 11.8 },
-        { RecordedOn: new Date(2023, 4,1,13,0,0), Temperature: 11.8, Temperature2: 11.7 },
-        { RecordedOn: new Date(2023, 4,1,14,0,0), Temperature: 12.8, Temperature2: 11.4 },
-        { RecordedOn: new Date(2023, 4,1,15,0,0), Temperature: 11.8, Temperature2: 11.2 },
-        { RecordedOn: new Date(2023, 4,1,16,0,0), Temperature: 10.8, Temperature2: 11.8 },
-      ];
+	  const data = await context.env.DATA.get("sensor_data");
+
+    const json = JSON.parse(data) as Array<{t1:number, t2:number, r:Date}>;
+    const readings : Record[] = json.map(j=> {return {Temperature: j.t1, Temperature2: j.t2, RecordedOn: j.r}});
  	return Response.json(readings);
 }
