77d6db9afc8f9c34883516b721be57ebac969873
CasperJ <2158013+CasperJ@users.noreply.github.com>
2025-05-21 08:32:27 +0200

    Update Chart.tsx to use temp1 as chart value

--- frontend/src/Chart.tsx
+++ frontend/src/Chart.tsx
@@ -137,7 +137,7 @@ class Chart extends Component<ChartProps, ChartState> {
       focus.select('#focusTextDeg')
         .attr('x', cx)
         .attr('y', cy - 15)
-        .html(`${dx.Temperature2}&deg;C`);
+        .html(`${dx.Temperature}&deg;C`);
       focus.select('#focusTextTime')
         .attr('x', cx)
         .attr('y', cy - 7)
@@ -166,7 +166,7 @@ class Chart extends Component<ChartProps, ChartState> {
     var areaGenerator = d3.area<any>()
       .x(function (d) { return x(d.RecordedOn) as number })
       .y0(y(0) as number)
-      .y1(function (d) { return y(d.Temperature2) as number }).curve(d3.curveBasis)
+      .y1(function (d) { return y(d.Temperature) as number }).curve(d3.curveBasis)
 
 
     // Add the area
